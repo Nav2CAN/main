31e6554980d3b308366e1fa17ec381c93b8499c1
Jonathan Eichild Schmidt <33545029+JonathanESchmidt@users.noreply.github.com>
2023-03-17 12:49:14 +0100

    added keypoints

--- docker-volume/ros_ws/pose_estimator/pose_estimator/person_keypoints.py
+++ docker-volume/ros_ws/pose_estimator/pose_estimator/person_keypoints.py
@@ -9,7 +9,7 @@ class keypoint():
     yImage=float
     x,y,z=float
 
-    def __init__(self, ID: str, xImage: float, yImage: float):
+    def __init__(self, ID: int, xImage: float, yImage: float):
 
         
         # np.where(self.IDnames=="neck")[0][0]
@@ -34,7 +34,25 @@ class person_keypoints:
                             "left_elbow", "right_elbow", "left_wrist",
                             "right_wrist", "left_hip", "right_hip", "left_knee",
                             "right_knee", "left_ankle", "right_ankle", "neck"])
-        self.nose=keypoint(0,0,0)
+        
+        self.nose=keypoint(0,None,None)
+        self.left_eye = keypoint(1, None, None)
+        self.right_eye = keypoint(2, None, None)
+        self.left_ear = keypoint(3, None, None)
+        self.right_ear = keypoint(4, None, None)
+        self.left_shoulder = keypoint(5, None, None)
+        self.right_shoulder = keypoint(6, None, None)
+        self.left_elbow = keypoint(7, None, None)
+        self.right_elbow = keypoint(8, None, None)
+        self.left_wrist = keypoint(9, None, None)
+        self.right_wrist = keypoint(10, None, None)
+        self.left_hip = keypoint(11, None, None)
+        self.right_hip = keypoint(12, None, None)
+        self.left_knee = keypoint(13, None, None)
+        self.right_knee = keypoint(14, None, None)
+        self.left_ankle = keypoint(15, None, None)
+        self.right_ankle = keypoint(16, None, None)
+        self.neck = keypoint(17, None, None)
 
         self.x=None
         self.y=None
