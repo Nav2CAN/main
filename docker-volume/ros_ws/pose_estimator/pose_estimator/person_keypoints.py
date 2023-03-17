8b61a0964bb8dfa12d43260c89c687ea1f4bb07b
Jonathan Eichild Schmidt <33545029+JonathanESchmidt@users.noreply.github.com>
2023-03-17 13:18:42 +0100

    Update person_keypoints.py

--- docker-volume/ros_ws/pose_estimator/pose_estimator/person_keypoints.py
+++ docker-volume/ros_ws/pose_estimator/pose_estimator/person_keypoints.py
@@ -9,7 +9,7 @@ class keypoint():
     yImage=float
     x,y,z=float
 
-    def __init__(self, ID: int, xImage: float, yImage: float):
+    def __init__(self, ID: str, xImage: float, yImage: float):
 
         
         # np.where(self.IDnames=="neck")[0][0]
@@ -26,37 +26,29 @@ class keypoint():
         self.y=0
         self.z=0
 
-
-class person_keypoints:
-    def __init__(self):
+class person_keypoint:
+    '''
+    Keypoints for one detection are passed to this object for calculation of 3D location and orientation
+    '''
+    def __init__(self, keypoints):
         self.IDnames = np.array(["nose", "left_eye", "right_eye", "left_ear",
                             "right_ear", "left_shoulder", "right_shoulder",
                             "left_elbow", "right_elbow", "left_wrist",
                             "right_wrist", "left_hip", "right_hip", "left_knee",
                             "right_knee", "left_ankle", "right_ankle", "neck"])
         
-        self.nose=keypoint(0,None,None)
-        self.left_eye = keypoint(1, None, None)
-        self.right_eye = keypoint(2, None, None)
-        self.left_ear = keypoint(3, None, None)
-        self.right_ear = keypoint(4, None, None)
-        self.left_shoulder = keypoint(5, None, None)
-        self.right_shoulder = keypoint(6, None, None)
-        self.left_elbow = keypoint(7, None, None)
-        self.right_elbow = keypoint(8, None, None)
-        self.left_wrist = keypoint(9, None, None)
-        self.right_wrist = keypoint(10, None, None)
-        self.left_hip = keypoint(11, None, None)
-        self.right_hip = keypoint(12, None, None)
-        self.left_knee = keypoint(13, None, None)
-        self.right_knee = keypoint(14, None, None)
-        self.left_ankle = keypoint(15, None, None)
-        self.right_ankle = keypoint(16, None, None)
-        self.neck = keypoint(17, None, None)
+        self.keypoints = np.array([])
+        for kp in keypoints:
+            self.keypoints = np.append(keypoints, keypoint(kp.ID, kp.x, kp.y))
+        
 
-        self.x=None
-        self.y=None
-        self.theta=None
+class person_tracking:
+    def __init__(self, x, y, theta):
+        
+
+        self.x = x
+        self.y = y
+        self.theta = theta
         self.xdot=None
         self.ydot=None
         self.thetadot=None
