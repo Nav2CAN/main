2097f914f196f25014af4bc1afdbab39e27081dc
Jonathan Eichild Schmidt <33545029+JonathanESchmidt@users.noreply.github.com>
2023-03-20 14:25:14 +0100

    CleanUp and Commenting functions

--- docker-volume/ros_ws/pose_estimator/pose_estimator/pose_estimator_node.py
+++ docker-volume/ros_ws/pose_estimator/pose_estimator/pose_estimator_node.py
@@ -1,17 +1,3 @@
-# Copyright 2016 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import rclpy
 import numpy as np
 import cv2
@@ -21,49 +7,32 @@ from sensor_msgs.msg import Image
 
 from cv_bridge import CvBridge
 
-import sys
-import argparse
 import jetson.inference
 import jetson.utils
 from jetson_inference import poseNet
-from jetson_utils import videoSource, videoOutput, logUsage
+from jetson_utils import videoOutput, logUsage
 
 import csv
 
 from .person_keypoints import *
 
 
-# @dataclass
-# class Pose:
-#     keypoints: list[float,float,float]
-#     ID: list[float]
-        
-#     def __init__(self, keypoints):
-
-#         self.keypoints=
-#         self.ID=
-
-
 class PoseEstimator(Node):
-
+    '''
+    Class for pose estimation of a person using Nvidia jetson implementation
+    of PoseNet and passing messages using ROS2.
+    The Class uses Intel Realsense messages on the ROS2 network as input for rgb and depth images
+    '''
 
     def __init__(self):
         super().__init__('pose_estimator')
 
-        # parser = argparse.ArgumentParser(description="Run pose estimation DNN on a video/image stream.", 
-        #                         formatter_class=argparse.RawTextHelpFormatter, 
-        #                         epilog=poseNet.Usage() + videoSource.Usage() + videoOutput.Usage() + logUsage())
-
-        # parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream")
-        # parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
-        # parser.add_argument("--network", type=str, default="resnet18-body", help="pre-trained model to load (see below for options)")
-        # parser.add_argument("--overlay", type=str, default="links,keypoints", help="pose overlay flags (e.g. --overlay=links,keypoints)\nvalid combinations are:  'links', 'keypoints', 'boxes', 'none'")
-        # parser.add_argument("--threshold", type=float, default=0.15, help="minimum detection threshold to use") 
-
+        # DC For data collection 
         self.peopleCount = 0
         self.imageCount = -1
         self.written = False
 
+        # for working with images in ROS2
         self.bridge=CvBridge()
         self.rgb=None
         self.cudaimage=None
@@ -84,19 +53,20 @@ class PoseEstimator(Node):
         self.rgb_subscription
         self.depth_subscription  # prevent unused variable warning
 
+        # Setup variables for PoseNet
         self.network = "resnet18-body"
         self.overlay = "links,keypoints,boxes"
         self.threshold = 0.15
         self.output_location = "images" # only needed for saving images
 
+        # Initialising PoseNet and its output
         self.net = poseNet(self.network, ['pose_estimator_node.py'], self.threshold)
-        # self.input = videoSource(self.rgb, argv=['pose_estimator_node.py'])
         self.output = videoOutput(self.output_location, argv=['pose_estimator_node.py'])
 
     def rgb_callback(self, msg):
         try:
             self.rgb = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-            cudaimage = cv2.cvtColor(self.rgb, cv2.COLOR_BGRA2RGBA).astype(np.float32)#converting the image to a cuda compatible image
+            cudaimage = cv2.cvtColor(self.rgb, cv2.COLOR_BGRA2RGBA).astype(np.float32) #converting the image to a cuda compatible image
             self.cudaimage = jetson.utils.cudaFromNumpy(cudaimage)
         except:
             pass
@@ -122,17 +92,21 @@ class PoseEstimator(Node):
             self.output.SetStatus("{:s} | Network {:.0f} FPS".format(self.network, self.net.GetNetworkFPS()))
             self.net.PrintProfilerTimes()
 
-
-    # def determine3DPose(self):
     def getPersons(self):
+        '''
+        Calculates the location of the person as X and Y coordinates along with the orientation of the person
+        '''
         persons = []
         for pose in self.poses:
-            kpPerson=person_keypoint(pose.Keypoints,self.depth)
+            kpPerson=person_keypoint(pose.Keypoints, self.depth)
             persons.append(kpPerson)
             
         return persons
     
     def writing(self, personlist):
+        '''
+        DC Function for writing csv file with person variables for captured images
+        '''
         with open('SanityCheck.csv', mode = 'a') as csvfile:
             writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)            
             
@@ -166,7 +140,7 @@ def main(args=None):
         while rclpy.ok():
             if(pose_estimator.cudaimage != None):
                 pose_estimator.detectPoses()
-            if pose_estimator.peopleCount == 10:
+            if pose_estimator.peopleCount == 10: # DC for data collection run only until a certain amount of people have been detected
                 break
             rclpy.spin_once(pose_estimator)
     except KeyboardInterrupt:
@@ -177,7 +151,6 @@ def main(args=None):
     # when the garbage collector destroys the node object)
     pose_estimator.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
