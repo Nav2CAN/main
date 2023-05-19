0a86d80b5db7a7d76d2ddeef49926755e4de2be0
Tristan Schwörer <tschwo21@student.aau.dk>
2023-05-19 14:23:35 +0200

    fixes

--- docker-volume/ros_ws/src/interaction_detection/interaction_detection/detectContextNode.py
+++ docker-volume/ros_ws/src/interaction_detection/interaction_detection/detectContextNode.py
@@ -97,10 +97,12 @@ class Detector(Node):
 
     def social_zone_callback(self, msg):
         try:
-            t = self.tf_buffer.lookup_transform(
-                "map",
-                msg.header.frame_id,
-                msg.header.stamp)
+            t = self.tf_buffer.lookup_transform_full(
+                target_frame="map",
+                target_time=rclpy.time.Time(),
+                source_frame=msg.header.frame_id,
+                source_time=msg.header.stamp,
+                fixed_frame="map")
         except TransformException as ex:
             self.get_logger().info(
                 f'Could not transform base_link to map: {ex}')
@@ -143,8 +145,8 @@ class Detector(Node):
                 bb = BoundingBox()
                 bb.header.stamp = msg.header.stamp
                 bb.header.frame_id = "map"
-                bb.center_x = t.transform.translation.x + x
-                bb.center_y = t.transform.translation.y - y
+                bb.center_x = x +t.transform.translation.x 
+                bb.center_y = -y + t.transform.translation.y
                 bb.width = w
                 bb.height = h
 
