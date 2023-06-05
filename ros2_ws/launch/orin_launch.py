760cb4d455dbf32b0dad90540e60a0f4324e91d9
Tristan Schwörer <tschwo21@student.aau.dk>
2023-06-05 13:08:29 +0200

    Orin changes

--- ros2_ws/launch/orin_launch.py
+++ ros2_ws/launch/orin_launch.py
@@ -1,5 +1,5 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetParameter
 from launch.actions import IncludeLaunchDescription
 from launch.substitutions import ThisLaunchFileDir
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -9,18 +9,25 @@ sys.path.append(str(pathlib.Path(__file__).parent.absolute()))
 
 def generate_launch_description():
     return LaunchDescription([
-        IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    [ThisLaunchFileDir(), '/rs_ours_camera1_launch.py']),
-            ),
+        SetParameter(name='use_sim_time', value=True),
         Node(
             package='multi_person_tracker',
             executable='multi_person_tracker',
             name='multi_person_tracker',
         ),
         Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            arguments=["0", ".055", "0", "-0.285",
+                       "0", "0", "camera_link", "camera1_link"]
+        ),
+        Node(
             package='interaction_detection',
             executable='interaction_detection',
             name='interaction_detection',
-        )
+        ),
+        IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [ThisLaunchFileDir(), '/rs_ours_camera1_launch.py']),
+            ),
     ])
